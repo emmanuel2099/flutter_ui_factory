5f4750b54979d183adf6b01610e61ab7dd0df537
Emma P. <emmappdesigner@gmail.com>
2025-06-14 22:50:47 +0100

    Prepare for publishing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 //
-// import 'package:untitled7/main.dart';
+// import 'package:untitled7/flutter_ui_factory.dart';
 //
 // void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
